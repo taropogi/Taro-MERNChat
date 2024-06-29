2c2184c8e5977fb1b6a2217f0a2664163ca12405
Richard Bernisca <taropogi_123@yahoo.com>
2024-06-30 02:02:22 +0800

    add route auth file

--- api/server.js
+++ api/server.js
@@ -1,10 +1,19 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
 const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+//routes
+import authRoutes from "./routes/auth.routes.js";
 
 app.get("/", (req, res) => {
   res.send("This is the API Server");
 });
 
-app.listen(5000, () => {
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
   console.log("App running");
 });
