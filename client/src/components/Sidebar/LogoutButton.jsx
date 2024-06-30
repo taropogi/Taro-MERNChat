fc06207f10b25ee73e736445432c011ca5b64fb2
Richard Bernisca <taropogi_123@yahoo.com>
2024-07-01 02:29:43 +0800

    add logout button

--- client/src/components/Sidebar/LogoutButton.jsx
+++ client/src/components/Sidebar/LogoutButton.jsx
@@ -1,3 +1,9 @@
+import { BiLogOut } from "react-icons/bi";
+
 export default function LogoutButton() {
-  return <div>LogoutButton</div>;
+  return (
+    <div className="mt-auto">
+      <BiLogOut className="w-6 h-6 text-white cursor-pointer" />
+    </div>
+  );
 }
--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import SearchInput from "./SearchInput";
 
 export default function Sidebar() {
   return (
-    <div>
+    <div className="border-r border-slate-500 p-4 flex flex-col">
       <SearchInput />
       <div className="divider px-3"></div>
       <Conversations />
