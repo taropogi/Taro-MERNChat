08eed9b6c78998b877544e7d5b957fce64270279
Richard Bernisca <taropogi_123@yahoo.com>
2024-07-01 01:41:26 +0800

    setup signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
-import Login from "./pages/login/Login";
+// import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center">
-      <Login />
+      <SignUp />
     </div>
   );
 }
--- client/src/pages/signup/SignUp.jsx
+++ client/src/pages/signup/SignUp.jsx
@@ -1,5 +1,81 @@
 import React from "react";
+import GenderCheckbox from "./GenderCheckbox";
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-20">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          Sign Up <span className="text-blue-500"> MERNChat</span>
+        </h1>
+        <form>
+          <div>
+            <label className="label">
+              <span className="text-base label-text">First Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter first name"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Last Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Last name"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Username"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter password"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Confirm Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter password"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+
+          <GenderCheckbox />
+
+          <a
+            href="#"
+            className="text-sm hover:underline hover:text-blue-600 mt-4 inline-block"
+          >
+            Already have an account?
+          </a>
+
+          <div>
+            <button className="btn btn-block btn-sm mt-2 border border-slate-700">
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
