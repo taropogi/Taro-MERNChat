b2021ed0661db6e09f1c0f88d942352a8eadfdb1
Richard Bernisca <taropogi_123@yahoo.com>
2024-07-02 01:18:30 +0800

    format colors in chat

--- client/src/components/messages/Message.jsx
+++ client/src/components/messages/Message.jsx
@@ -1,19 +1,30 @@
 import { useAuth } from "../../contextProviders/AuthContext";
+import useConversation from "../../zustand/useConversation";
 export default function Message({ message }) {
   const { authUser } = useAuth();
-  const isSender = authUser._id === message.senderId;
+  const isSenderMe = authUser._id === message.senderId;
+  const {
+    selectedConversation: { profilePic: toChatProfilePic },
+  } = useConversation();
   return (
-    <div className={`chat chat-${isSender ? "end" : "start"}`}>
+    <div className={`chat chat-${isSenderMe ? "end" : "start"}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img alt="User Avatar" src={authUser.profilePic} />
+          <img
+            alt="User Avatar"
+            src={isSenderMe ? authUser.profilePic : toChatProfilePic}
+          />
         </div>
       </div>
       <div className="chat-header">
         You
         <time className="text-xs opacity-50">12:45</time>
       </div>
-      <div className="chat-bubble">{message.message}</div>
+      <div
+        className={`chat-bubble ${isSenderMe ? "bg-blue-500 text-white" : ""}`}
+      >
+        {message.message}
+      </div>
       <div className="chat-footer opacity-50">Delivered</div>
     </div>
   );
