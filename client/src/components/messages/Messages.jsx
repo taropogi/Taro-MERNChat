42d2b0a88e208d84e783d0c4ec1d3ebb94525862
Richard Bernisca <taropogi_123@yahoo.com>
2024-07-08 23:26:30 +0800

    remove loading

--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -35,8 +35,7 @@ export default function Messages({ isTyping }) {
   const isToChatOnline = onlineUsers.includes(receiverId);
   return (
     <div className="px-4 flex-1 overflow-auto ">
-      {!isLoading &&
-        messages.length > 0 &&
+      {messages.length > 0 &&
         messages.map((message, i) => (
           <div
             key={message._id}
