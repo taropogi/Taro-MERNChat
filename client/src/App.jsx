63d78b0db8ff887e69e973da86ccda25dda9b05e
Richard Bernisca <taropogi_123@yahoo.com>
2024-07-15 22:11:52 +0800

    spinner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,46 @@
-import Login from "./pages/login/Login";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home";
-import SignUp from "./pages/signup/SignUp";
 import { Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { useAuth } from "./contextProviders/AuthContext";
 import { ChatProvider } from "./contextProviders/ChatContext";
+import { lazy, Suspense } from "react";
+import SpinnerFullPage from "./components/SpinnerFullPage/SpinnerFullPage";
+// import Home from "./pages/home/Home";
+// import SignUp from "./pages/signup/SignUp";
+// import Login from "./pages/login/Login";
+
+const Home = lazy(() => import("./pages/home/Home"));
+const SignUp = lazy(() => import("./pages/signup/SignUp"));
+const Login = lazy(() => import("./pages/login/Login"));
+
 function App() {
   const { authUser } = useAuth();
   return (
     <div className="p-4 h-screen flex items-center">
-      <Routes>
-        <Route
-          path="/"
-          element={
-            authUser ? (
-              <ChatProvider className={`mx-auto`}>
-                <Home />
-              </ChatProvider>
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/login"
-          element={authUser ? <Navigate to="/" /> : <Login />}
-        />
-        <Route
-          path="/signup"
-          element={authUser ? <Navigate to="/" /> : <SignUp />}
-        />
-      </Routes>
+      <Suspense fallback={<SpinnerFullPage />}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              authUser ? (
+                <ChatProvider className={`mx-auto`}>
+                  <Home />
+                </ChatProvider>
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/login"
+            element={authUser ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/signup"
+            element={authUser ? <Navigate to="/" /> : <SignUp />}
+          />
+        </Routes>
+      </Suspense>
       <div>
         <Toaster />
       </div>
